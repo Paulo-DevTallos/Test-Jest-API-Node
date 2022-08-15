e3ea6a71e6c8eb9a51862b2a8cae716adf253e71
Paulo Sergio <pauloserg861@gmail.com>
2022-08-15 15:08:14 -0300

    implementando testes unitarios

--- src/test/models/editora.test.js
+++ src/test/models/editora.test.js
@@ -1,88 +1,68 @@
+import { describe, expect, it, jest } from '@jest/globals';
+import Editora from '../../models/editora';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import {
-  describe, expect, it, jest,
-} from '@jest/globals';
-import Editora from '../../models/editora.js';
-
-describe('Testando o modelo Editora', () => {
+describe('Testando o modelo editora', () => {
   const objetoEditora = {
     nome: 'CDC',
-    cidade: 'Sao Paulo',
-    email: 'c@c.com',
+    cidade: 'São Paulo',
+    email: 'c@c.com.br',
   };
 
   it('Deve instanciar uma nova editora', () => {
+    const editoria = new Editora(objetoEditora);
+
+    expect(editoria).toEqual(
+      expect.objectContaining(objetoEditora) //esperamos que esse objeto contenha
+    ) 
+  })
+
+  //testando funções asincronas
+  it.skip('Deve salvar editora no banco de dados', () => {
     const editora = new Editora(objetoEditora);
 
-    expect(editora).toEqual(
-      expect.objectContaining(objetoEditora),
-    );
-  });
-
-  it.skip('Deve salvar editora no BD', () => {
-    const editora = new Editora(objetoEditora);
-
-    editora.salvar().then((dados) => {
-      expect(dados.nome).toBe('CDC');
+    editora.salvar().then(dados => {
+      expect(dados.nome).toBe('CDC'); 
     });
   });
 
-  it.skip('Deve salvar no BD usando a sintaxe moderna', async () => {
-    const editora = new Editora(objetoEditora);
+  it.skip('Deve salvar no banco usando a sintaxe moderna', async () => {
+    const editra = new Editora(objetoEditora);
 
-    const dados = await editora.salvar();
+    const dados = await editra.salvar()
 
-    const retornado = await Editora.pegarPeloId(dados.id);
+    const retornado = await Editora.pegarPeloId(dados.id)
 
     expect(retornado).toEqual(
-      expect.objectContaining({
+      expect.objectContaining({ 
         id: expect.any(Number),
         ...objetoEditora,
         created_at: expect.any(String),
         updated_at: expect.any(String),
-      }),
-    );
-  });
+       })
+    )
+  })
 
-  it('Deve fazer uma chamada simulada ao BD', () => {
-    const editora = new Editora(objetoEditora);
+  it('Deve fazer uma chamada simulada ao DB', () => {
+    const editora = new Editora(objetoEditora)
 
-    editora.salvar = jest.fn().mockReturnValue({
+    editora.salvar = jest.fn().mockReturnValue({ 
       id: 10,
       nome: 'CDC',
-      cidade: 'Sao Paulo',
-      email: 'c@c.com',
-      created_at: '2022-10-01',
-      updated_at: '2022-10-01',
-    });
+      cidade: 'São Paulo',
+      email: 'c@c.com.br',
+      created_at: '2022-02-02',
+      updated_at: '2022-06-01',
+    }); //retorna um valor especifico
 
     const retorno = editora.salvar();
 
     expect(retorno).toEqual(
-      expect.objectContaining({
+      expect.objectContaining({ 
         id: expect.any(Number),
         ...objetoEditora,
         created_at: expect.any(String),
         updated_at: expect.any(String),
-      }),
-    );
-  });
-});*/
+       })
+    )
+  })
+});
